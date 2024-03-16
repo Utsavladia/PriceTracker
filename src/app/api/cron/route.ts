ddde20cc80563aa3d2e646f53cc714a7b4bb6802
Utsavladia <utsavladia@gmail.com>
2024-03-16 16:40:00 +0530

    build fixes

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -7,7 +7,7 @@ import { scraper } from "../../../../lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "../../../../lib/utils";
 
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
